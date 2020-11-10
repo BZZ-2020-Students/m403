ee7f40b28f7bf98287f4acb54f4359813fd4ba50
Tim Irmler - Laptop <42088586+zwazel@users.noreply.github.com>
2020-11-10 09:09:09 +0100

    jup

--- universe/universe.java
+++ universe/universe.java
@@ -6,6 +6,8 @@
   @version 0.1
 */
 
+import java.util.Scanner;
+
 public class universe { // NOTE public = jeder hat zugriff darauf
   /*
     Starts the execution
@@ -22,6 +24,18 @@ public class universe { // NOTE public = jeder hat zugriff darauf
     runs the program
   */
   private void run() { // Private = Keine andere Klasse hat darauf zugriff // Void = kein return
-    // TODO realize the program logic
+    String text; // NOTE Init String
+    int repeat; // NOTE Init int
+
+    Scanner scanner = new Scanner(System.in); // NOTE create new Scanner object called scanner
+    System.out.print("Welcher Text soll angezeigt werden? "); // NOTE tell user what he has to do
+    text = scanner.nextLine(); // NOTE get the input and store it in the variable
+
+    System.out.print("Wie oft soll der Text angezeigt werden? "); // NOTE tell user what he has to do
+    repeat = Integer.parseInt(scanner.nextLine()); // NOTE get the input and store it in the variable (as integer)
+
+    for (int i = 0; i < repeat; i++) { // NOTE Repeat for as long as the user said we have to
+      System.out.println(text); // NOTE gebe Text aus
+    }
   }
 }

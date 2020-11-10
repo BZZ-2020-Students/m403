472a426ab8e3e201502188e844f36557863b463d
Tim Irmler - Laptop <42088586+zwazel@users.noreply.github.com>
2020-11-10 08:27:49 +0100

    Create Template.java

---
+++ templateJava/Template.java
@@ -0,0 +1,15 @@
+/*
+  template for a simple java program
+
+  @author Tim Irmler (Zwazel)
+  @since 2020-11-10
+  @version 0.1
+*/
+
+// Programme werden als Klassen bezeichnet
+// Klassenname und Dateiname müssen gleich sein!
+public class Template { // Template ist der Name der Klasse
+  public static void main(String[] args) { // Jedes Programm braucht diese Methode, main ist die die als erstes ausgeführt wird
+    
+  }
+}

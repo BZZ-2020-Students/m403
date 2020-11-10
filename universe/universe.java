68a729f28993d12f1bbfe9ceea9a7c0b2bc06f9e
Tim Irmler - Laptop <42088586+zwazel@users.noreply.github.com>
2020-11-10 08:44:07 +0100

    renamed folder and removed comments

---
+++ universe/universe.java
@@ -0,0 +1,27 @@
+/*
+  
+
+  @author Tim Irmler (Zwazel)
+  @since 2020-11-10
+  @version 0.1
+*/
+
+public class template {
+  /*
+    starts the execution
+
+    @param args command line arguments
+  */
+  public static void main(String[] args) {
+    template program = new template();
+    program.run();
+
+/*
+  runs the program
+*/
+
+  private void run() {
+    // TODO realize the program logic
+  }
+
+}

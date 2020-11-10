17dd6a088fba33d392928b24a8a336290308a129
Tim Irmler - Laptop <42088586+zwazel@users.noreply.github.com>
2020-11-10 08:41:45 +0100

    Update Template.java

--- templateJava/Template.java
+++ templateJava/Template.java
@@ -6,7 +6,7 @@
   @version 0.1
 */
 
-//  NOTE Programme werden als Klassen bezeichnet
+// NOTE Programme werden als Klassen bezeichnet
 // NOTE Klassenname und Dateiname müssen gleich sein!
 public class template { // Template ist der Name der Klasse
   /*

b630ac7c9626ee80a696d2ab7daa5b9acc3c79d6
Tim Irmler - Gamer PC <zwazel@hotmail.com>
2020-11-23 18:10:58 +0100

    Create HoelzchenSpiel.java

---
+++ tim_stuff/hoelzchenSpiel/HoelzchenSpiel.java
@@ -0,0 +1,55 @@
+package hoelzchenSpiel;
+
+import java.util.Scanner;
+
+public class HoelzchenSpiel {
+    Scanner scanner = new Scanner(System.in);
+
+    int anzahlHoelzchen = 21;
+    int currentPlayer = 1;
+
+    final int maxHoelzchen = 3;
+    final int minHoelzchen = 1;
+
+    public static void main(String[] args) {
+        HoelzchenSpiel program = new HoelzchenSpiel();
+        program.run();
+    }
+
+    public void run() {
+        while (anzahlHoelzchen > 0) {
+            int takeHoelzchen = howManyHoelzchen();
+
+            while (takeHoelzchen > maxHoelzchen || takeHoelzchen < minHoelzchen) {
+                System.out.println("Du darfst nicht mehr/weniger Hölzchen als " + minHoelzchen + " bzw " + maxHoelzchen + " nehmen!");
+                takeHoelzchen = howManyHoelzchen();
+            }
+
+            anzahlHoelzchen -= takeHoelzchen;
+            switchPlayer();
+            System.out.print("\n\n");
+        }
+
+        switchPlayer();
+        System.out.print("\n\n");
+        System.out.println("Spieler " + currentPlayer + " hat verloren!!");
+    }
+
+    private int howManyHoelzchen() {
+        System.out.println("Es sind " + anzahlHoelzchen + " Hölzer vorhanden");
+        System.out.println("Spieler " + currentPlayer + ": Wieviele Hölzer nimmst du? (1-3) ");
+        return scanner.nextInt();
+    }
+
+    private void switchPlayer() {
+        switch(currentPlayer) {
+            case 1:
+                currentPlayer = 2;
+                break;
+
+            case 2:
+                currentPlayer = 1;
+                break;
+        }
+    }
+}

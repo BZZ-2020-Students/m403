30b8551a32a2a0a3290ff2aebc4e2c9864817772
Marc <marconymous@outlook.com>
2020-12-01 09:48:43 +0100

    Update Muenzenspiel.java

--- marc_stuff/Muenzenspiel/Muenzenspiel.java
+++ marc_stuff/Muenzenspiel/Muenzenspiel.java
@@ -1,28 +1,90 @@
-/* M403
-*
-*@author Marc Andri Fuchs
-*@since 2020-11-10
-*@version 0.1
-*/
+import java.util.Scanner;
 
-public class Muenzenspiel {
+/**
+ * play a game with coins
+ *
+ * @author Marcel Suter
+ * @since 2018-09-28
+ * @version 1.0
+ */
+public class Muenzenspiel{
 
-  /*
-  * starts the execution
-  * @param args
-  */
-  public static void main(String[] args) {
-    Muenzenspiel program = new Muenzenspiel();
-    System.out.println("Gud Ebening!\n\n");
-    program.run();
-    System.out.println("Gud Bye!");
-  }
+    static Scanner scanner;
 
-  /**/
-  private void run() {
-    int coinsAmount = (int)Math.random() * 20 + 10;
-    System.out.println(coinsAmount);
+    /**
+     * constructor
+     */
+    public Muenzenspiel() {
+        scanner = new Scanner(System.in);
+    }
 
+    /**
+     * starts the execution
+     *
+     * @param args
+     *            command line arguments
+     */
+    public static void main(String[] args) {
+        Muenzenspiel program = new Muenzenspiel();
 
-  }
+        System.out.println("Willkommen zum Münzenspiel");
+        program.play();
+        System.out.println("Bis bald");
+        scanner.close();
+    }
+
+    /**
+     * play the coin game
+     */
+    private void play() {
+        // TODO Variablen deklarieren
+        int currentPlayer = 1;
+        int coinsAmount = 0;
+        int coinsTaken = 0;
+
+        /* generate random number of coins
+           between 10 and 30 */
+        coinsAmount = (int)(Math.random() * 20 + 10);
+
+        while (coinsAmount > 0 ) {
+          do {
+            System.out.println("Currently there are " + coinsAmount + " coins!");
+            System.out.print("Player 1: How many coins do you want to take (1-3): ");
+            coinsTaken = scanner.nextInt();
+            if (coinsTaken < 1) {
+              System.out.println("You need to take at least 1 coin!");
+            } else if (coinsTaken > 3) {
+              System.out.println("You can only take a maximum of 3 coins!");
+            } else {
+              coinsAmount -= coinsTaken;
+            }
+            currentPlayer = 1;
+          } while (coinsTaken < 1 || coinsTaken > 3);
+          if (coinsAmount < 1) {
+            break;
+          }
+
+          do {
+            System.out.println("Currently there are " + coinsAmount + " coins!");
+            System.out.print("Player 2: How many coins do you want to take (1-3): ");
+            coinsTaken = scanner.nextInt();
+            if (coinsTaken < 1) {
+              System.out.println("Player 2: You need to take at least 1 coin!");
+            } else if (coinsTaken > 3) {
+              System.out.println("Player 2: You can only take a maximum of 3 coins!");
+            }
+            else {
+              coinsAmount -= coinsTaken;
+            }
+            currentPlayer = 2;
+          } while (coinsTaken < 1 || coinsTaken > 3);
+        }
+
+        if (currentPlayer == 1) {
+          System.out.println("Player 1 wins!");
+        }
+        else if (currentPlayer == 2) {
+          System.out.println("Player 2 wins!");
+        }
+    }
 }

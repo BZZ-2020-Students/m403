bc8195af42982a1ded969197b2816756fea47c62
Marc <marconymous@outlook.com>
2021-01-12 11:09:05 +0100

    beeg updeit

--- marc_stuff/m403Project/src/lengthconvertor/DistanceConverter.java
+++ marc_stuff/m403Project/src/lengthconvertor/DistanceConverter.java
@@ -10,6 +10,12 @@ import java.util.Scanner;
  * @since 2018-12-10
  */
 public class DistanceConverter {
+    double length;
+    int unitFrom;
+    int unitTo;
+    String[] units;
+    double[][] values;
+    double result;
 
     // declare attributes
     static Scanner scanner;
@@ -19,6 +25,18 @@ public class DistanceConverter {
      */
     public DistanceConverter() {
         scanner = new Scanner(System.in);
+        length = 0;
+        unitFrom = -1;
+        unitTo = -1;
+        length = 0;
+        units = new String[]{"meters", "miles", "nautical miles", "yards", "inches"};
+        values = new double[][]{
+                {1.0, 0.000621371, 0.0005399, 1.09361, 39.3701},
+                {1609.34, 1.0, 0.868976, 1760.0, 63360.0},
+                {1852.216, 1.15078, 1.0, 2025.37, 72913.4},
+                {0.9144, 0.000568182, 0.000493737, 1.0, 36},
+                {0.0254, 0.000015783, 0.000013715, 0.0277778, 1.0},
+        };
     }
 
     /**
@@ -36,68 +54,75 @@ public class DistanceConverter {
      * runs the converter
      */
     private void run() {
-        int unitFrom = -1;
-        int unitTo = -1;
-        double length = 0;
+        getLength();
+        printInfo();
+        getUnitFrom();
+        getUnitTo();
+        calculateResult();
+        printResult();
+    }
 
-        String[] units = new String[]{"meters", "miles", "nautical miles", "yards", "inches"};
-        double[][] values = new double[][]{
-                {1.0, 0.000621371, 0.0005399, 1.09361, 39.3701},
-                {1609.34, 1.0, 0.868976, 1760.0, 63360.0},
-                {1852.216, 1.15078, 1.0, 2025.37, 72913.4},
-                {0.9144, 0.000568182, 0.000493737, 1.0, 36},
-                {0.0254, 0.000015783, 0.000013715, 0.0277778, 1.0},
-        };
-
-
-        while (length <= 0) {
-            System.out.print("Länge >");
+    private void getLength() {
+        while (this.length <= 0) {
+            System.out.print("Length > ");
             while (!scanner.hasNextDouble()) {
-                System.out.println("Länge muss eine positive Zahl sein.");
+                System.out.println("The length needs to be a positive number!");
                 scanner.nextLine();
-                System.out.print("Länge >");
+                System.out.print("Length > ");
             }
-            length = scanner.nextDouble();
-            if (length <= 0) {
-                System.out.println("Länge muss eine positive Zahl sein.");
-                length = 0;
+            this.length = scanner.nextDouble();
+            if (this.length <= 0) {
+                System.out.println("The length needs to be a positive number!");
+                this.length = 0;
             }
         }
+    }
 
-        System.out.println("Zulässige Einheiten für die Umrechnung:");
+    private void getUnitFrom() {
+        while (this.unitFrom == -1) {
+            System.out.print("Unit from (1-5) > ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Unknown Unit. Please enter 1-5.");
+                scanner.nextLine();
+                System.out.print("Unit from (1-5) > ");
+            }
+            this.unitFrom = scanner.nextInt();
+            if (this.unitFrom < 1 || this.unitFrom > 5) {
+                this.unitFrom = -1;
+                System.out.println("Unknown Unit. Please enter 1-5.");
+            }
+        }
+    }
+
+    private void getUnitTo() {
+        while (this.unitTo == -1) {
+            System.out.print("Unit to (1-5) > ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Unknown Unit. Please enter 1-5.");
+                scanner.nextLine();
+                System.out.print("Unit to (1-5) > ");
+            }
+            this.unitTo = scanner.nextInt();
+            if (this.unitTo < 1 || this.unitTo > 5) {
+                this.unitTo = -1;
+                System.out.println("Unknown Unit. Please enter 1-5.");
+            }
+        }
+    }
+
+    private void printInfo() {
+        System.out.println("Allowed Units for conversion : ");
         for (int i = 0; i < units.length; i++) {
             System.out.print(units[i] + " = " + (i + 1) + ", ");
         }
         System.out.println();
-        while (unitFrom == -1) {
-            System.out.print("Einheit von (1-5) >");
-            while (!scanner.hasNextInt()) {
-                System.out.println("Unbekannte Einheit. Bitte 1-5 eingeben.");
-                scanner.nextLine();
-                System.out.print("Einheit von (1-5) >");
-            }
-            unitFrom = scanner.nextInt();
-            if (unitFrom < 1 || unitFrom > 5) {
-                unitFrom = -1;
-                System.out.println("Unbekannte Einheit. Bitte 1-5 eingeben.");
-            }
-        }
+    }
 
-        while (unitTo == -1) {
-            System.out.print("Einheit nach (1-5) >");
-            while (!scanner.hasNextInt()) {
-                System.out.println("Unbekannte Einheit. Bitte 1-5 eingeben.");
-                scanner.nextLine();
-                System.out.print("Einheit nach (1-5) >");
-            }
-            unitTo = scanner.nextInt();
-            if (unitTo < 1 || unitTo > 5) {
-                unitTo = -1;
-                System.out.println("Unbekannte Einheit. Bitte 1-5 eingeben.");
-            }
-        }
+    private void calculateResult() {
+        this.result = length * values[unitFrom - 1][unitTo - 1];
+    }
 
-        double result = length * values[unitFrom - 1][unitTo - 1];
+    private void printResult() {
         System.out.println(length + " " + units[unitFrom - 1] + " in " + units[unitTo - 1] + " is: " + result);
     }
 }

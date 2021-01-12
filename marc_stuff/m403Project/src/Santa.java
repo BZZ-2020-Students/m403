fb67b5be0c3421f1dc17c116843df465752c2cb6
Marc <marconymous@outlook.com>
2021-01-12 08:43:42 +0100

    e hehe santa do be naughty v2

--- marc_stuff/m403Project/src/Santa.java
+++ marc_stuff/m403Project/src/Santa.java
@@ -23,13 +23,10 @@ public class Santa {
     public static void main(String[] args) {
         Santa program = new Santa();
         double total = 0;
-        double points = -999;
         System.out.println("Herzliche Willkommen, lieber Nikolaus");
-
 
         total = program.deeds();
         program.verdict(total);
-
 
         scanner.close();
     }
@@ -45,6 +42,10 @@ public class Santa {
         while (total >= -20 && points != 0) {
             scanner = new Scanner(System.in);
             System.out.println("Enter the amount of points the CHILD gets (-6 to 6) : ");
+            while (!scanner.hasNextDouble()) {
+                System.out.println("This is not a valid number. Try again > ");
+                scanner.next();
+            }
             points = scanner.nextDouble();
             total += points;
             if (points >= 0) {
@@ -53,7 +54,13 @@ public class Santa {
                 System.out.println("A bad deed!");
             }
         }
+        total = roundPoints(total);
         return total; // FIXME return the total points
+    }
+
+    private double roundPoints(double input) {
+        int rounded = (int) ((input + 0.25) * 2);
+        return rounded / 2f;
     }
 
     /**
@@ -62,6 +69,7 @@ public class Santa {
      * @param points the total points accumulated
      */
     private void verdict(double points) {
+        System.out.println("This child has " + points + " points!");
         if (points < 0)
             System.out.println("A naughty child");
         else if (points < 9.5)
